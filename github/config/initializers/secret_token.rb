1769cfbe611d581845d73674ce0d7e8365e10bad
OGATA Syohei <ogata@hoccs.co.jp>
2012-10-16 11:06:48 +0900

    rails new github

---
+++ github/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Github::Application.config.secret_token = '49757e7390435b64165c1d55e1242f9fa77556879fa040283a908db6d5c9cba27298597385c625712a01645f9f8a69146d12fced8c0a2ad3c74a92c793b88f4b'
